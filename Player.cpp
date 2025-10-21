1faf956729ce568b2d4583609d94143f0b041515
Mariam-Mohamedali <mariammohamedali127@gmail.com>
2025-10-21 15:33:49 +0300

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -17,7 +17,7 @@ Player::Player()
     timeSlider.setRange(0.0, 1.0, 0.01);
     timeSlider.setValue(0.0); //start from time: 0.0 sec
     speedSlider.addListener(this);
-    speedSlider.setRange(0.25, 2.0, 0.01);
+    speedSlider.setRange(0.05, 2.0, 0.01);
     speedSlider.setValue(1.0); 
     addAndMakeVisible(volumeSlider);
     addAndMakeVisible(timeSlider);
