dd57cfcc9ec3fde91c04608345b0ddc602d15b12
Egymario06 <mario.nasser2006@gmail.com>
2025-11-02 00:19:31 +0200

    Update Player.h

--- Player.h
+++ Player.h
@@ -5,7 +5,8 @@ class Player : public juce::AudioAppComponent,
     public juce::Button::Listener,
     public juce::Slider::Listener,
     public juce::Timer,
-    public juce::ChangeListener
+    public juce::ChangeListener,
+    public juce::ComboBox::Listener
 {
 public:
     Player();
@@ -17,12 +18,13 @@ public:
     // Event handlers
     void buttonClicked(juce::Button* button) override;
     void sliderValueChanged(juce::Slider* slider) override;
+    void comboBoxChanged(juce::ComboBox* comboBox) override;
 
     // Audio control
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
     void releaseResources();
-    
+
     // Time
     void timerCallback() override;
 
@@ -37,20 +39,21 @@ private:
 
     // GUI
     juce::TextButton loadButton{ "Load" };
-    juce::TextButton playButton{ "Play ►" };
+    juce::TextButton playButton{ "Play |>" };
     juce::TextButton pauseButton{ "Pause ||" };
     juce::TextButton restartButton{ "Restart" };
-    juce::TextButton startButton{ "|◄" };
-    juce::TextButton endButton{ "►|" };
+    juce::TextButton startButton{ "|<|" };
+    juce::TextButton endButton{ "|>|" };
     juce::TextButton stopButton{ "Stop" };
     juce::TextButton loopButton{ "Loop" };
     juce::TextButton muteButton{ "Mute" };
     juce::TextButton loopStartEndButton{ "values to loop" };
+    juce::TextButton loadPlaylistButton{ "Load playlist" };
     juce::TextEditor setStart;
     juce::TextEditor setEnd;
     juce::TextEditor repeat_times;
     double startPoint, endPoint;
-	bool isLooping;
+    bool isLooping;
     bool is_restartLoop;
     int repeatedTimes;
     bool isMuted = false;
@@ -59,10 +62,13 @@ private:
     juce::Slider timeSlider;
     juce::Slider speedSlider;
     juce::Label metadataLable;
-    juce::AudioThumbnailCache thumbnailCache{10};
-    juce::AudioThumbnail thumbnail{512, formatManager, thumbnailCache};
-
-
+    juce::AudioThumbnailCache thumbnailCache{ 10 };
+    juce::AudioThumbnail thumbnail{ 512, formatManager, thumbnailCache };
+    juce::ComboBox playlistBox;
+    juce::Array<juce::File> playlistFiles;
+    int currentTrackIndex = -1;
+    void loadPlaylistFiles(const juce::Array<juce::File>& files);
+    void selectTrack(int index);
 
     std::unique_ptr<juce::FileChooser> fileChooser;
 
